78e3d4d537d7282e0fc69f42939272b805a3ba50
audrey <ayou332@aucklanduni.ac.nz>
2023-03-15 00:07:34 +1300

    Added instance field numberOfProfiles and initialised it in the constructor

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -3,12 +3,18 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+  private int numberOfProfiles;
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
+    numberOfProfiles = 0;
   }
 
   public void printDatabase() {
-    // TODO: Complete this method.
+    String strNumberOfProfiles = String.valueOf(numberOfProfiles);
+    if (numberOfProfiles == 0) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(strNumberOfProfiles, "s",".");
+    }
   }
 
   public void createNewProfile(String userName, String age) {
