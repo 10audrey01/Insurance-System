a8bf9beb769440389d8d093be1ea6ef674dac4fd
audrey <ayou332@aucklanduni.ac.nz>
2023-03-15 20:04:12 +1300

    Changed arraylist profileAges type from Integer to String

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -6,13 +6,13 @@ import nz.ac.auckland.se281.Main.PolicyType;
 public class InsuranceSystem {
   private int numberOfProfiles;
   private ArrayList<String> profileNames;
-  private ArrayList<Integer> profileAges;
+  private ArrayList<String> profileAges;
 
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
     numberOfProfiles = 0;
     profileNames = new ArrayList<String>();
-    profileAges = new ArrayList<Integer>();
+    profileAges = new ArrayList<String>();
   }
 
   public void printDatabase() {
