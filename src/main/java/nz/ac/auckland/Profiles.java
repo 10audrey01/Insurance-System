1677b1aa62ef4247063e200587eb2ccfa5341115
audrey <ayou332@aucklanduni.ac.nz>
2023-03-16 22:31:48 +1300

    Created Profiles class and initialised instance variables

---
+++ src/main/java/nz/ac/auckland/Profiles.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland;
+
+public class Profiles {
+  private String userName;
+  private String age;
+
+  public Profiles(String userName, String age) {
+    this.userName = userName;
+    this.age = age;
+  }
+}
