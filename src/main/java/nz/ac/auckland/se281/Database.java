b87ffc080ee72b1bab92246b5f0f96b6fcbda246
audrey <ayou332@aucklanduni.ac.nz>
2023-03-16 22:45:20 +1300

    Added class Database and initialised instance variables with constructor

---
+++ src/main/java/nz/ac/auckland/se281/Database.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se281;
+
+import java.util.ArrayList;
+
+public class Database {
+  private int numberOfProfiles;
+  private ArrayList<String> profiles;
+
+  public Database() {
+    numberOfProfiles = 0;
+    profiles = new ArrayList<String>();
+  }
+}
