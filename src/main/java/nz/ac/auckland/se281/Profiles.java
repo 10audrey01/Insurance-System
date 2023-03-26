574fbb834727bea0be0700ba4f9de70b881de71b
audrey <ayou332@aucklanduni.ac.nz>
2023-03-26 16:06:43 +1300

    Created instance field profileLoaded and a setter method for it

--- src/main/java/nz/ac/auckland/se281/Profiles.java
+++ src/main/java/nz/ac/auckland/se281/Profiles.java
@@ -3,10 +3,12 @@ package nz.ac.auckland.se281;
 public class Profiles {
   private String userName;
   private String age;
+  private boolean profileLoaded;
 
   public Profiles(String userName, String age) {
     this.userName = userName;
     this.age = age;
+    this.profileLoaded = false;
   }
 
   public String getUserName() {
@@ -15,5 +17,9 @@ public class Profiles {
 
   public String getAge() {
     return age;
+  }
+
+  public void setProfileLoadedToTrue() {
+    this.profileLoaded = true;
   }
 }
